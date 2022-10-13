cfdec339e87917617400d891fae4b0922b519474
bhavya <2022mt93171@wilp.bits-pilani.ac.in>
2022-10-13 14:59:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bhavya's First project on React to save on GitHub
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
